37a614d944926d73ac865186d10fb1abf258861d
Jamie Weatherby <jamie.weatherby@points.com>
2020-10-22 21:45:37 -0400

    Finalize get endpoints

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import random
 from flask import Flask, jsonify
 from api.handlers import (
     get_tax_brackets
@@ -11,9 +12,17 @@ app = Flask(__name__)
 @app.errorhandler(404)
 def not_found_handler(e):
     return jsonify({
-        'errors': format_error('That url was not found', code='NOT_FOUND')
+        'errors': format_error(
+            'That url was not found',
+            code='NOT_FOUND'
+        )
     }), 404
 
+@app.errorhandler(Exception)
+def exception_handler(e):
+    return jsonify({
+        'errors': format_error(str(e), code='INTERNAL_SERVER_ERROR')
+    }), 500
 
 
 @app.route('/')
@@ -33,9 +42,14 @@ def default_brackets():
 
 @app.route('/tax-brackets/<tax_year>')
 def tax_year_brackets(tax_year):
-    try:
-        resp_dict = get_tax_brackets(tax_year)
-    except Exception as e:
-        return jsonify({'errors': format_error(str(e))}), 400
 
-    return jsonify(resp_dict)
+    # be evil
+    # import pdb; pdb.set_trace()
+    roulette = random.randint(0, 4)
+    print(f'Roulette {roulette} (3 is bad today)')
+    if roulette == 3:
+        raise Exception("Database not found!")
+
+    tax_brackets = get_tax_brackets(tax_year)
+
+    return jsonify(tax_brackets)
